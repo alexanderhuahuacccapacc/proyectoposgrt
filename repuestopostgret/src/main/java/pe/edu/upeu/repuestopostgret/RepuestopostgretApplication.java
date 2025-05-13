5da35f53e30afd783d254d4fcd220635c8c5c9f6
alexanderhuahuacccapacc <alexander123huahuaccapa@gmail.com>
2025-05-13 15:27:58 -0500

    posgret_1

---
+++ repuestopostgret/src/main/java/pe/edu/upeu/repuestopostgret/RepuestopostgretApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.repuestopostgret;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepuestopostgretApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepuestopostgretApplication.class, args);
+	}
+
+}
---
+++ repuestopostgret/src/test/java/pe/edu/upeu/repuestopostgret/RepuestopostgretApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.repuestopostgret;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepuestopostgretApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
